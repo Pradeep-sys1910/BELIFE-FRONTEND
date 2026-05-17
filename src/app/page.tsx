69b33e2bc7937b84027fb045e95cac3268f8c3e2
Pradeep <paytym333@gmail.com>
2026-05-17 13:34:38 +0530

    Security: remove react-quill (XSS), framer-motion (unused), upgrade postcss to 8.5.10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-import HomeContent from '@/components/home/HomeContent';
+import dynamic from 'next/dynamic';
+
+const HomeContent = dynamic(() => import('@/components/home/HomeContent'), {
+  ssr: false,
+});
 
 export default function HomePage() {
   return <HomeContent />;
